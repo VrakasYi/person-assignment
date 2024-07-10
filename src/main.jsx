6709b22d78fe3556edeb0681106044224b63f32a
VrakasYi <vrakasyi@outlook.com>
2024-07-10 07:50:57 +0300

    person.jsx created and imported

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Person from './person.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    < Person />
   </React.StrictMode>,
 )
