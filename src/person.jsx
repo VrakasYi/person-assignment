34689f740ced57a7518efa82a779988db2c4243e
VrakasYi <vrakasyi@outlook.com>
2024-07-10 16:34:29 +0300

    fullname const

--- src/person.jsx
+++ src/person.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 
 function Person() {
     const [person, setPerson] = useState({ name: "John", age: 100 });
+    const [firstName, setFirstName] = useState("John");
+    const [lastName, setLastName] = useState("");
+    const fullName = firstName + " " + lastName;
 
     const handleIncreaseAge = () => {
         console.log("in handleIncreaseAge (before setPerson call): ", person);
@@ -10,15 +13,30 @@ function Person() {
         console.log("in handleIncreaseAge (after setPerson call): ", person);
     };
 
+    function firstNameInput(event) {
+        setFirstName(event.target.value)
+    }
+
+    function lastNameInput(event) {
+        setLastName(event.target.value)
+    }
+
     // this console.log runs every time the component renders
     // what do you think this will print?
     console.log("during render: ", person);
 
     return (
         <>
-        <h1>{person.name}</h1>
+        <h1>{fullName}</h1>
         <h2>{person.age}</h2>
         <button onClick={handleIncreaseAge}>Increase age</button>
+        <br/>
+        <input placeholder="First name" 
+            onChange={firstNameInput}
+        />
+        <input placeholder="Last name" 
+            onChange={lastNameInput}
+        />
         </>
     );
 }
